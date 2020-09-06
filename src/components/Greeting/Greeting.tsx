f91cd1ed168e78fd69352e73d1dfbc9fc21ce314
妹尾弦 <gengineer.web@gmail.com>
2020-09-06 21:29:10 +0900

    :heavy_plus_sign: emotion 導入

--- src/components/Greeting/Greeting.tsx
+++ src/components/Greeting/Greeting.tsx
@@ -1,12 +1,19 @@
+import { css } from '@emotion/core'
 import React from 'react'
 
 type Props = {
   target: string
 }
 
+const test = css({
+  color: `red`,
+})
+
 export const Component: React.FC<Props> = (props) => (
   <h1 className="text-5xl text-center text-accent-1">
     Hello, <span className="text-teal-600">{props.target}</span>
+    Hello, <span css={css({ fontSize: `100px` })}>{props.target}</span>
+    Hello, <span css={test}>{props.target}</span>
   </h1>
 )
 
