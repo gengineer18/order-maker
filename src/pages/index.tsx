1e272219b914e0718fe3b24c7ecea6d389a0a6c3
妹尾弦 <gengineer.web@gmail.com>
2020-09-10 18:35:07 +0900

    🚧functionレスポンス取得確認

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,22 @@
 import { HomeContainer } from '@/components/container/home'
+import axios from 'axios'
 
-export default function HomePage() {
-  return <HomeContainer />
+export default function HomePage({ res }) {
+  return (
+    <>
+      <p>{res}</p>
+      <HomeContainer />
+    </>
+  )
+}
+
+export async function getStaticProps() {
+  const response = await axios.get('https://asia-northeast1-order-maker.cloudfunctions.net/api/hello')
+  const data = await response.data
+  const res = await data.res
+  return {
+    props: {
+      res,
+    },
+  }
 }
