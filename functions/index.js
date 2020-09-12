f009d886c83cabfc557ed7813476c8145b573437
妹尾弦 <gengineer.web@gmail.com>
2020-09-12 17:24:49 +0900

    ✨ cloud functionsを通じたデータのselect, insertを実装

--- functions/index.js
+++ functions/index.js
@@ -1,5 +1,10 @@
+/* eslint-disable import/order */
 const functions = require('firebase-functions')
 const mysql = require('mysql')
+const dayjs = require('dayjs')
+
+dayjs.locale('ja')
+
 const https = functions.region('asia-northeast1').https
 
 const connectionName = functions.config().db.connection
@@ -21,59 +26,172 @@ if (process.env.NODE_ENV === 'production') {
 
 let mysqlPool
 
-exports.mysqlDemo = https.onRequest((req, res) => {
+exports.getAllPost = https.onRequest(async (req, res) => {
+  res.header('Access-Control-Allow-Origin', '*')
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept')
+
   if (!mysqlPool) {
     mysqlPool = mysql.createPool(mysqlConfig)
   }
 
-  res.set('Access-Control-Allow-Origin', 'http://localhost:3000')
-  res.set('Access-Control-Allow-Methods', 'GET, HEAD, OPTIONS, POST')
-  res.set('Access-Control-Allow-Headers', 'Content-Type, authorization')
-
-  mysqlPool.query('select * from users', (err, results) => {
-    if (err) {
-      console.error(err)
-      res.status(500).send(err)
-    } else {
-      res.send(JSON.stringify(results))
-    }
+  const connection = await new Promise((resolve, reject) => {
+    mysqlPool.getConnection((error, connection) => {
+      if (error) reject(error)
+      resolve(connection)
+    })
   })
+
+  try {
+    const results = await new Promise((resolve, reject) => {
+      const query = 'select * from posts'
+      connection.query(query, (error, results) => {
+        if (error) {
+          reject(error)
+        }
+        resolve(results)
+      })
+    })
+
+    res.status(200).send(JSON.stringify(results))
+  } catch (err) {
+    await new Promise((resolve, reject) => {
+      connection.rollback((error, results) => {
+        if (error) reject(error)
+        resolve(results)
+      })
+    })
+    console.error('!!!!!!!!!!!!!GET ERROR!!!!!!!!!!!!!')
+    console.error(err)
+    res.status(500).send(err)
+  } finally {
+    connection.release()
+    mysqlPool.end()
+  }
 })
 
-// exports.mysqlDemo2 = https.onRequest(async (req, res) => {
-//   if (!mysqlPool) {
-//     mysqlPool = mysql.createPool(mysqlConfig)
-//   }
+exports.insertPost = https.onRequest(async (req, res) => {
+  res.header('Access-Control-Allow-Origin', '*')
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept')
 
-//   res.set('Access-Control-Allow-Origin', 'http://localhost:3000')
-//   res.set('Access-Control-Allow-Methods', 'GET, HEAD, OPTIONS, POST')
-//   res.set('Access-Control-Allow-Headers', 'Content-Type, authorization')
+  if (!mysqlPool) {
+    mysqlPool = mysql.createPool(mysqlConfig)
+  }
 
-//   const min = 10000000000
-//   const max = 99999999999
+  const connection = await new Promise((resolve, reject) => {
+    mysqlPool.getConnection((error, connection) => {
+      if (error) reject(error)
+      resolve(connection)
+    })
+  })
 
-//   const randomId = Math.floor(Math.random() * (max + 1 - min)) + min
+  const {
+    userName = '',
+    title = '',
+    position1,
+    position2,
+    position3,
+    position4,
+    position5,
+    position6,
+    position7,
+    position8,
+    position9,
+    description1 = '',
+    description2 = '',
+    description3 = '',
+    description4 = '',
+    description5 = '',
+    description6 = '',
+    description7 = '',
+    description8 = '',
+    description9 = '',
+  } = req.query
+  const dateStr = dayjs().format('MMssYYDDmm')
+  const max = 999
+  const min = 100
+  const random = String(Math.floor(Math.random() * (max + 1 - min)) + min)
+  const postId = `ODR${random}${dateStr}`
 
-//   await mysqlPool.query(
-//     'insert into users (id, firstName, familyName) values (?, ?, ?)',
-//     [randomId, 'test', 'user'],
-//     (err, results) => {
-//       console.log('test1', results)
-//       if (err) {
-//         console.log('test2', results)
-//         mysqlPool.rollback(() => mysqlPool.release())
-//       } else {
-//         mysqlPool.commit((err) => {
-//           if (err) {
-//             console.log('test3', results)
-//             mysqlPool.rollback(() => mysqlPool.release())
-//           } else {
-//             console.log('test4', results)
-//             mysqlPool.release()
-//           }
-//         })
-//       }
-//     }
-//   )
-//   res.send({ data: 'hoge' })
-// })
+  try {
+    const results = await new Promise((resolve, reject) => {
+      connection.query(
+        'insert into posts (postId, userName, title) values (?, ?, ?)',
+        [postId, userName, title],
+        (error, results) => {
+          if (error) {
+            reject(error)
+          }
+          resolve(results)
+        }
+      )
+      const query =
+        'insert into postData (`postId`, `order`, `position`, `description`) values (?, ?, ?, ?), (?, ?, ?, ?), (?, ?, ?, ?), (?, ?, ?, ?), (?, ?, ?, ?), (?, ?, ?, ?), (?, ?, ?, ?), (?, ?, ?, ?), (?, ?, ?, ?)'
+      const data = [
+        postId,
+        1,
+        position1,
+        description1,
+        postId,
+        2,
+        position2,
+        description2,
+        postId,
+        3,
+        position3,
+        description3,
+        postId,
+        4,
+        position4,
+        description4,
+        postId,
+        5,
+        position5,
+        description5,
+        postId,
+        6,
+        position6,
+        description6,
+        postId,
+        7,
+        position7,
+        description7,
+        postId,
+        8,
+        position8,
+        description8,
+        postId,
+        9,
+        position9,
+        description9,
+      ]
+      connection.query(query, data, (error, results) => {
+        if (error) {
+          reject(error)
+        }
+        resolve(results)
+      })
+    })
+
+    await new Promise((resolve, reject) => {
+      connection.commit((error, results) => {
+        if (error) reject(error)
+        resolve(results)
+      })
+    })
+
+    res.status(200).send(JSON.stringify(results))
+  } catch (err) {
+    await new Promise((resolve, reject) => {
+      connection.rollback((error, results) => {
+        if (error) reject(error)
+        resolve(results)
+      })
+    })
+    console.error('!!!!!!!!!!!!!INSERT ERROR!!!!!!!!!!!!!')
+    console.error(err)
+    res.status(500).send(err)
+  } finally {
+    connection.release()
+    mysqlPool.end()
+  }
+})
