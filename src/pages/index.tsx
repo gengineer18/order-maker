e8ea65597305b77fbe982078a686b9632a45413a
妹尾弦 <gengineer.web@gmail.com>
2020-09-12 17:26:26 +0900

    🚧全投稿データの取得

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,18 +1,27 @@
 import { HomeContainer } from '@/components/container/home'
+import { TPost } from '@/types/PostData'
 import axios from 'axios'
+import { GetStaticProps } from 'next'
+import React from 'react'
 
-export default function HomePage({ data }) {
+export default function HomePage({ data }: { data: TPost[] }) {
   return (
     <>
-      <p>{data.firstName}</p>
+      {data.map((data) => (
+        <React.Fragment key={data.postId}>
+          <p>{data.postId}</p>
+          <p>{data.title}</p>
+          <p>{data.userName}</p>
+        </React.Fragment>
+      ))}
       <HomeContainer />
     </>
   )
 }
 
-export async function getStaticProps() {
-  const response = await axios.get('https://asia-northeast1-order-maker.cloudfunctions.net/mysqlDemo')
-  const data = await response.data[0]
+export const getStaticProps: GetStaticProps = async () => {
+  const response = await axios.get('https://asia-northeast1-order-maker.cloudfunctions.net/getAllPost')
+  const data: TPost[] = await response.data
   return {
     props: {
       data,
